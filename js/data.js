1530f21dde74aefac316a483865060c16ae558c6
DAVID\Mincho Stamenov <mstamenov@david.bg>
2025-01-26 07:39:29 +0200

    Move inline product items in its own file

---
+++ js/data.js
@@ -0,0 +1,62 @@
+const products = [
+        {
+            name: "Продукт 1",
+            price: 199,
+            image: "https://pulsefit.bg/uploads/tinymce/58069.jpg"
+        },
+        {
+            name: "Продукт 2",
+            price: 299,
+            image: "https://jkfitness.bg/image/cache/catalog/TOORX/Kombiniran-fitnes-ured-MSX-50-TOORX-jkfitness.bg%20(6)-900x900.jpg"
+        },
+        {
+            name: "Продукт 3",
+            price: 399,
+            image: "https://frankfurt.apollo.olxcdn.com/v1/files/lmxovp8ho052-BG/image"
+        },
+        {
+            name: "Продукт 4",
+            price: 499,
+            image: "https://cdn.technomarket.bg/ng/media/cache/mid_thumb/uploads/marketplace/43/650711-1.jpg.webp"
+        },
+        {
+            name: "Продукт 5",
+            price: 599,
+            image: "https://cdncloudcart.com/32450/products/images/18955/veloergometar-k-fit-mount-image_6406909b70057_600x600.jpeg?1678151842"
+        },
+        {
+            name: "Продукт 6",
+            price: 699,
+            image: "https://nadaspa.eu/cdn/shop/products/1_5f3c21c4-1727-43b9-9a6e-efe3ea7d78d6_720x1000.jpg?v=1606237369"
+        },
+        {
+            name: "Продукт 7",
+            price: 799,
+            image: "https://sporti.bg/g/30/komplekt-dambeli-20-kg.webp"
+        },
+        {
+            name: "Продукт 8",
+            price: 899,
+            image: "https://homeco.bg/media/catalog/product/cache/520332c47eb5e742430b01c96310ed79/F/A/FA1002-0.jpg"
+        },
+        {
+            name: "Продукт 9",
+            price: 999,
+            image: "https://homeco.bg/media/catalog/product/cache/520332c47eb5e742430b01c96310ed79/F/A/FA1002-0.jpg"
+        },
+        {
+            name: "Продукт 10",
+            price: 1099,
+            image: "https://via.placeholder.com/280"
+        },
+        {
+            name: "Продукт 11",
+            price: 1199,
+            image: "https://via.placeholder.com/280"
+        },
+        {
+            name: "Продукт 12",
+            price: 1299,
+            image: "https://via.placeholder.com/280"
+        }    
+]
